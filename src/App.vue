8ec7887ae7ee8b5c6d1858fbb9a3d59ca90e57cc
Konstantin <konvod93@gmail.com>
2026-02-03 17:14:28 +0200

    refactoring: created TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import TheHeader from './components/TheHeader.vue'
 
 const navItems = {
   timeline: ClockIcon,
@@ -11,21 +12,7 @@ const navItems = {
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#" class="text-sm">
-      <img src="./assets/logo.png" alt="Logo" class="h-9" />
-    </a>
-    <a href="#">
-      <div v-if="true" class="flex items-center gap-1">
-        Day complete!
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-      <div v-else class="flex items-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-        <div class="h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-    </a>
-  </header>
+ <TheHeader />
   <main class="flex grow flex-col">
 
   </main>

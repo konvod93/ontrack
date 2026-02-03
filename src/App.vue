a43bfcb1c11f3bd7bc403e255ee8aececc596ac1
Konstantin <konvod93@gmail.com>
2026-02-03 17:24:58 +0200

    refactoring: created ThrNav component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
 
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import TheHeader from './components/TheHeader.vue'
-
-const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  stats: ChartBarIcon,
-}
+import TheNav from './components/TheNav.vue'
 
 </script>
 
@@ -16,13 +10,5 @@ const navItems = {
   <main class="flex grow flex-col">
 
   </main>
-  <nav class="sticky bottom-0 z-10 bg-white">
-    <ul class="flex items-center justify-around border-t">
-      <li v-for="icon, page in navItems" :key="page" class="flex-1">
-        <a v-bind:href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-        <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <TheNav />
 </template>

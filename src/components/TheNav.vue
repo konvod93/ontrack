db161058f82fb5730fa8ce1b064bcde33f7b0e4a
Konstantin <konvod93@gmail.com>
2026-02-04 16:26:28 +0200

    refactoring: created NavItem component

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from './NavItem.vue'
 
 const navItems = {
   timeline: ClockIcon,
@@ -11,13 +12,11 @@ const navItems = {
 </script>
 
 <template>
-<nav class="sticky bottom-0 z-10 bg-white">
+  <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="icon, page in navItems" :key="page" class="flex-1">
-        <a v-bind:href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+      <NavItem v-for="icon, page in navItems" :key="page" v-bind:href="`#${page}`">
         <component :is="icon" class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
+      </NavItem>
     </ul>
   </nav>
 </template>

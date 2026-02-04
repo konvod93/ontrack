54feb8171edfe0bcd22cdda6636a90158555649f
Konstantin <konvod93@gmail.com>
2026-02-04 17:36:39 +0200

    highlighting of active menu item

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,6 +2,7 @@
 
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import NavItem from './NavItem.vue'
+import { ref } from 'vue'
 
 const navItems = {
   timeline: ClockIcon,
@@ -9,12 +10,19 @@ const navItems = {
   stats: ChartBarIcon,
 }
 
+const currentPage = ref('timeline')
+
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="icon, page in navItems" :key="page" v-bind:href="`#${page}`">
+      <NavItem
+      v-for="icon, page in navItems"
+      :key="page"
+      v-bind:href="`#${page}`"
+      @click="currentPage = page"
+      :class="{ 'bg-gray-200 pointer-events-none' : page === currentPage }">
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>

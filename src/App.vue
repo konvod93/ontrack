7e6cff10253548551e774d26b76373cfa0ccde27
Konstantin <konvod93@gmail.com>
2026-02-22 15:13:59 +0200

    activities creation

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,10 @@ function goTo(page) {
   currentPage.value = page
 }
 
+function createActivity(activity) {
+  activities.value.push(activity)
+}
+
 function deleteActivity(activity) {
  activities.value.splice(activities.value.indexOf(activity), 1)
 }
@@ -29,9 +33,17 @@ function deleteActivity(activity) {
 <template>
   <TheHeader @navigate="goTo($event)" />
   <main class="flex flex-col grow">
-    <TheTimeLine v-show="currentPage === PAGE_TIMELINE" :timeline-items="timelineItems"
-      :activity-select-options="activitySelectOptions" />
-    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" :activities="activities" @delete-activity="deleteActivity" />
+    <TheTimeLine
+      v-show="currentPage === PAGE_TIMELINE"
+      :timeline-items="timelineItems"
+      :activity-select-options="activitySelectOptions"
+    />
+    <TheActivities
+      v-show="currentPage === PAGE_ACTIVITIES"
+      :activities="activities"
+      @create-activity="createActivity"
+      @delete-activity="deleteActivity"
+    />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
   <TheNav :current-page="currentPage" @navigate="goTo($event)" />
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -6,7 +6,10 @@ import { PlusIcon } from '@heroicons/vue/24/outline'
 
 const emit = defineEmits({
   deleteActivity: isActivityValid,
+  createActivity: isActivityValid,
 })
+
+let newActivity = ''
 
 defineProps({
   activities: {
@@ -27,8 +30,14 @@ defineProps({
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
-    <form class="sticky bottom-14.25 flex gap-2 border-t bg-white p-4">
-      <input type="text" class="w-full rounded border px-4 text-xl"  placeholder="Activity name"/>
+    <form @submit.prevent ="emit('createActivity', newActivity)"  class="sticky bottom-14.25 flex gap-2 border-t bg-white p-4">
+      <input
+        type="text"
+        :value="newActivity"
+        @input="newActivity = $event.target.value"
+        class="w-full rounded border px-4 text-xl"
+        placeholder="Activity name"
+      />
       <BaseButton>
         <PlusIcon class="h-8" />
       </BaseButton>

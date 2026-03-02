28939558008294965dc34152c96f68ddc615e8ef
Konstantin <konvod93@gmail.com>
2026-03-02 17:32:19 +0200

    added seconds to activity complete component

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -2,6 +2,7 @@
 import BaseButton from './BaseButton.vue'
 import { TrashIcon } from '@heroicons/vue/24/outline'
 import BaseSelect from './BaseSelect.vue'
+import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
 import { isActivityValid, isUndefined, isNumber } from '@/validators.js'
 import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '@/constants.js'
 
@@ -27,14 +28,15 @@ defineProps({
       </BaseButton>
       <span class="text-xl truncate">{{ activity.name }}</span>
     </div>
-    <div>
+    <div class="flex gap-2">
       <BaseSelect
-        class="font-mono"
+        class="font-mono grow"
         placeholder="hh:mm"
         :selected="activity.secondsToComplete || null"
         :options="PERIOD_SELECT_OPTIONS"
         @select="emit('setSecondsToComplete', $event || 0)"
       />
+      <ActivitySecondsToComplete v-if="activity.secondsToComplete" :activity="activity" />
     </div>
   </li>
 </template>

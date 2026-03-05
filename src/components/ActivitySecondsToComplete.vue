51ef1a6f7ccfe72b6ac808016f7f556d055495b1
Konstantin <konvod93@gmail.com>
2026-03-05 14:17:38 +0200

    fixing bugs

--- src/components/ActivitySecondsToComplete.vue
+++ src/components/ActivitySecondsToComplete.vue
@@ -16,9 +16,7 @@ const props = defineProps({
   }
 })
 
-const classes = computed(
-  () => `flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600 ${colorClasses.value}`
-)
+const classes = computed(() => `flex items-center rounded px-2 font-mono text-xl ${colorClasses.value}`)
 const colorClasses = computed(() => (secondsDiff.value < 0 ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600'))
 const seconds = computed(() => `${sign.value}${formatSeconds(secondsDiff.value)}`)
 const sign = computed(() => (secondsDiff.value >= 0 ? '+' : '-'))

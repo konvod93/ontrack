e691c1dbf350d497bfb165a3c2171023f43ce4a1
Konstantin <konvod93@gmail.com>
2026-03-08 16:39:08 +0200

    access to deleteActivity function using provide/inject

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -4,12 +4,8 @@ import BaseButton from './BaseButton.vue'
 import { TrashIcon } from '@heroicons/vue/24/outline'
 import BaseSelect from './BaseSelect.vue'
 import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
-import { isActivityValid, isUndefined } from '@/validators.js'
+import { isActivityValid } from '@/validators.js'
 import { BUTTON_TYPE_DANGER } from '@/constants.js'
-
-const emit = defineEmits({
-  delete: isUndefined
-})
 
 defineProps({
   activity: {
@@ -21,12 +17,13 @@ defineProps({
 
 const setActivitySecondsToComplete = inject('setActivitySecondsToComplete')
 const periodSelectOptions = inject('periodSelectOptions')
+const deleteActivity = inject('deleteActivity')
 </script>
 
 <template>
   <li class="flex flex-col p-4 gap-2">
     <div class="flex items-center gap-2">
-      <BaseButton :type="BUTTON_TYPE_DANGER" @click="emit('delete')">
+      <BaseButton :type="BUTTON_TYPE_DANGER" @click="deleteActivity(activity)">
         <TrashIcon class="h-8" />
       </BaseButton>
       <span class="text-xl truncate">{{ activity.name }}</span>

6977c6142bfec9edfc54159e2b478f764f78a451
Konstantin <konvod93@gmail.com>
2026-03-31 17:58:39 +0300

    added timeline indicator

--- src/pages/TheTimeLine.vue
+++ src/pages/TheTimeLine.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TimelineItem from '../components/TimelineItem.vue'
+import TheTimelineIndicator from '@/components/TheTimelineIndicator.vue'
 import { timelineItems, timelineItemRefs, scrollToCurrentHour } from '@/timeline-items'
 import { onActivated, onMounted } from 'vue'
 
@@ -8,7 +9,8 @@ onActivated(scrollToCurrentHour)
 </script>
 
 <template>
-  <div class="mt-7">
+  <div class="relative mt-7">
+    <TheTimelineIndicator />
     <ul>
       <TimelineItem
         v-for="timelineItem in timelineItems"

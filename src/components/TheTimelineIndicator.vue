29e9d00920f60582375b116186b09aa5992d54cf
Konstantin <konvod93@gmail.com>
2026-03-31 18:32:08 +0300

    added dynamically change of timeline indicator position

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,19 +1,32 @@
 <script setup>
-import { HUNDRED_PERCENT, MINUTES_IN_HOUR, SECONDS_IN_DAY, SECONDS_IN_MINUTE } from '@/constants'
-import { ref } from 'vue'
+import {
+  HUNDRED_PERCENT,
+  MILLISECONDS_IN_SECOND,
+  MINUTES_IN_HOUR,
+  SECONDS_IN_DAY,
+  SECONDS_IN_MINUTE
+} from '@/constants'
+import { computed, ref, watchEffect } from 'vue'
+
+const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
 
 const indicatorRef = ref(null)
 
-function calculateTopOffset() {
-  return (calculateSecondsSinceMidnightInPercentage() * getTimelineHeight()) / HUNDRED_PERCENT
-}
+setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECOND)
 
-function calculateSecondsSinceMidnightInPercentage() {
-  return (HUNDRED_PERCENT * calculateSecondsSinceMidnight()) / SECONDS_IN_DAY
-}
+const topOffset = computed(() => (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT)
+
+const secondsSinceMidnightInPercentage = computed(() => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY)
+
+watchEffect(() => {
+  if (secondsSinceMidnight.value >= SECONDS_IN_DAY) {
+    secondsSinceMidnight.value = 0
+  }
+})
 
 function calculateSecondsSinceMidnight() {
   const now = new Date()
+
   return SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() + SECONDS_IN_MINUTE * now.getMinutes() + now.getSeconds()
 }
 
@@ -26,6 +39,6 @@ function getTimelineHeight() {
   <hr
     ref="indicatorRef"
     class="pointer-events-none absolute z-10 w-full border-b-2 border-red-600"
-    :style="{ top: `${calculateTopOffset()}px` }"
+    :style="{ top: `${topOffset}px` }"
   />
 </template>

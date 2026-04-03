2f910c1c10aa4d88309b13b87d1c8acbd8b0d20a
Konstantin <konvod93@gmail.com>
2026-04-03 18:23:53 +0300

    added saving app state in local storage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import './assets/main.css'
-
+import * as storage from './storage'
 import { createApp } from 'vue'
 import App from './App.vue'
+import { timelineItems } from './timeline-items'
+import { activities } from './activities'
+
+document.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'hidden') {
+    console.log('hidden')
+    saveState()
+  }
+})
+
+function saveState() {
+  storage.save({
+    timelineItems: timelineItems.value,
+    activities: activities.value
+  })
+}
 
 createApp(App).mount('#app')
 
--- src/time.js
+++ src/time.js
@@ -1,7 +1,10 @@
 import { computed, ref } from 'vue'
 import { HUNDRED_PERCENT, MILLISECONDS_IN_SECOND, SECONDS_IN_DAY, SECONDS_IN_MINUTE } from '@/constants'
 
-export const now = ref(new Date())
+const date = new Date()
+date.setHours(0, 0)
+
+export const now = ref(date)
 
 export const secondsSinceMidnightInPercentage = computed(
   () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
@@ -14,7 +17,7 @@ const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLI
 let timer = null
 
 export function startTimer() {
-  now.value = new Date()
+  now.value = date
   timer = setInterval(() => {
     now.value = new Date(now.value.getTime() + SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND)
   }, MILLISECONDS_IN_SECOND)

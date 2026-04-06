c8ee640d5177ab0836670059e6c2911131a7d9cb
Konstantin <konvod93@gmail.com>
2026-04-06 17:38:57 +0300

    fixed bug in storage.js (DATE/date)

--- src/storage.js
+++ src/storage.js
@@ -7,7 +7,7 @@ import { isToday } from './time'
 export function loadState() {
   const serializedState = localStorage.getItem(APP_NAME)
   const state = serializedState ? JSON.parse(serializedState) : {}
-  timelineItems.value = isToday(new Date(state.DATE)) ? state.timelineItems : timelineItems.value
+  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
   activities.value = state.activities || activities.value
   //activities.value = isToday(new Date(state.DATE)) ? state.activities : activities.value
 }
